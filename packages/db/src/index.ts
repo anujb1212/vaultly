1349327da94e1e34cf54144e4bca4f9e83c98b0f
Anuj Bajpai <anujbajpai.1212@gamil.com>
2025-08-14 21:12:57 +0000

    bootstrapped the project

---
+++ packages/db/src/index.ts
@@ -0,0 +1,18 @@
+export * from '@prisma/client';
+
+import { NextResponse } from "next/server"
+import { PrismaClient } from "../generated/prisma"
+
+const client = new PrismaClient();
+
+export const GET = async () => {
+    await client.user.create({
+        data: {
+            email: "asd",
+            name: "adsads"
+        }
+    })
+    return NextResponse.json({
+        message: "hi there"
+    })
+}
