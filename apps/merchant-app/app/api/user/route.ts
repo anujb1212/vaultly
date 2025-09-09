8800747b83f6c819fd2177ce86c56226a1cc5695
Anuj Bajpai <anujbajpai.1212@gmail.com>
2025-09-09 20:55:46 +0000

    added workflow changes and dockerfile

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,15 +1,33 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 import prisma from "@repo/db/client";
+import { Prisma } from "@prisma/client";
 
 export const GET = async () => {
-    await prisma.merchant.create({
-        data: {
-            email: "asd",
-            name: "adsads",
-            auth_type: "Google"
+    try {
+        await prisma.merchant.create({
+            data: {
+                email: "asd",
+                name: "adsads",
+                auth_type: "Google",
+            },
+        });
+
+        return NextResponse.json({
+            message: "hi there",
+        });
+    } catch (error) {
+        if (
+            error instanceof Prisma.PrismaClientKnownRequestError &&
+            error.code === "P2002" // unique constraint violation code
+        ) {
+            return NextResponse.json(
+                { error: "Email already exists" },
+                { status: 409 }
+            );
         }
-    })
-    return NextResponse.json({
-        message: "hi there"
-    })
-}
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
+    }
+};
